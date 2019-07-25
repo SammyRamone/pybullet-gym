6ed3a012f4d2d8e866a95df412307aeaa072f493
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2019-07-25 17:32:36 +0200

    added plane scene and wolfgang robot

---
+++ pybulletgym/envs/roboschool/scenes/plane.py
@@ -0,0 +1,11 @@
+import os,  inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+os.sys.path.insert(0,parentdir)
+
+from .scene_bases import Scene
+import pybullet
+
+
+class StadiumScene(Scene):
+	multiplayer = False
